1310a003b177383231d8ad598c1c22586cac117e
Ameekh <shaik.ameekh@gmail.com>
2023-05-14 23:56:00 +0530

    Git Checkout

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -5,7 +5,7 @@ def call(Map stageParams) {
         branches: [[name:  stageParams.branch ]],
         userRemoteConfigs: [[ url: stageParams.url ]]
         // userRemoteConfigs: [[ credentialsId: stageParams.credentialsId ]]
-        withCredentials ([usernamePassword(credentialsId: 'PAT_Git_Jenkins')])
+        withCredentials ([credentialsId: 'PAT_Git_Jenkins'])
                         // Your code using the GitHub credentials
                         // sh 'git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/your/repository.git'
                         // Additional steps using the credentials
